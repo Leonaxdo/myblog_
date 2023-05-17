5426a6c67971135d1339d863426424852448e744
Seungjun Lee <108439947+Leonaxdo@users.noreply.github.com>
2023-05-17 17:20:47 +0900

    Add files via upload

---
+++ web_blog/main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+__all__ = ['Post']  # 이 줄을 추가하세요.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
